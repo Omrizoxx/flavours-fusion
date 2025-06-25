3b86a1f01de416fa70586aca097a5f7a8aed85cb
Omari <omaradan091@gmail.com>
2025-06-26 00:31:15 +0300

    fought tooth and nail on this one

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+let myName = "Omari";
+console.log(myName)
+
+let fruits = ["mangos", "bananas", "avocados", "grapes"]
+
+console.log(fruits[0])
+fruits.push("ginger")
+console.log(fruits)
+fruits.unshift("casava")
+console.log(fruits)
